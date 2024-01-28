9254844c12dcb04f81b862d5c3b65c529e336416
Austin Wallace <awallace43@gatech.edu>
2024-01-28 06:23:55 -0500

    import errors

--- src/docking_tools_amw/__init__.py
+++ src/docking_tools_amw/__init__.py
@@ -1,9 +1,9 @@
 from . import prepare_gpf
 from . import prepare_receptor4
 from . import prepare_ligand4
-from . import amber_tools
-from . import rdkit_tools
 try:
     from . import mda_tools
+    from . import amber_tools
+    from . import rdkit_tools
 except ImportError:
     pass
